7632cb762b5ac7210c0b8b3d4ed36fd18aecc1a0
lao9 <3399.shao@gmail.com>
2018-06-19 00:43:54 +0800

    Add create game button

---
+++ GameCode/util.js
@@ -0,0 +1,13 @@
+Util = function () {
+    this.addButton = function (btnImage, xx, yy, f, game, scope) {
+        var button = game.add.sprite(xx, yy, btnImage).setInteractive();
+        button.setScale(0.5, 0.5);
+
+        button.inputEnabled = true;
+        button.on('pointerdown', (event) =>{
+            f(event, scope);
+        });
+        //button.events.onInputDown.add(f, scope);
+        return button;
+    }
+}
